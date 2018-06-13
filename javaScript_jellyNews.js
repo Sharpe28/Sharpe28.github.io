1282b37eeb0d118ab1ef6e48b6bb90fbf0d3c043
Sharpe28 <36647107+Sharpe28@users.noreply.github.com>
2018-06-13 14:42:19 -0700

    Add files via upload

---
+++ javaScript_jellyNews.js
@@ -0,0 +1,10 @@
+window.alert("I can see the things you doin' And you think that I'm naive But when I get the goods on you She'll finally believe She says it's all just drama But every bubble's got to pop She's gonna see just what you're doin' And then you're finally gonna have to stop! Don't think you're gonna win this time Cause' you better believe I'm gonna drop a dime On you I'll get ya! Yeah! [2x] And when I do you're gonna be busted! Busted! I don't wanna put the hurt on you But you better believe me when I tell you That I finally got the dirt on you You're busted! (busted!) Yeah, she's finally gonna see your lie This is how it's gonna be When she finds out that I was always right!");
+function homicideFunction() {
+    document.getElementById("homicide").innerHTML = " the big corporate Kraft was shut down due to illegal Jell-O production. According to Darylll S.Parker, a Swat officer whose squad had raided the Kraft factory, 'We infiltrated the factory around 11:29am, where we found the perpetrators using illegal ingredient 'gelatin'... there were about 48 workers. My squad was able to arrest 39 workers,2 of which were undocumented illegal citizens'. The CEO of Kraft will have a press conference on May 20th, where Ms. Rosenfeld is said to release an appology. Although, she has made it clear that she will not be answering any further questions.";
+}
+function camoflaugeFunction() {
+    document.getElementById("homicide").innerHTML = "Paragraph changed.";
+}
+function rebelFunction() {
+    document.getElementById("homicide").innerHTML = "Paragraph changed.";
+}
