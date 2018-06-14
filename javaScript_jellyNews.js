1b69fda9296177d0016bb402fb8c20b86f811517
Sharpe28 <36647107+Sharpe28@users.noreply.github.com>
2018-06-14 11:40:01 -0700

    Add files via upload

--- javaScript_jellyNews.js
+++ javaScript_jellyNews.js
@@ -1,10 +1,20 @@
+// JavaScript Alert once webpage is opened
 window.alert("I can see the things you doin' And you think that I'm naive But when I get the goods on you She'll finally believe She says it's all just drama But every bubble's got to pop She's gonna see just what you're doin' And then you're finally gonna have to stop! Don't think you're gonna win this time Cause' you better believe I'm gonna drop a dime On you I'll get ya! Yeah! [2x] And when I do you're gonna be busted! Busted! I don't wanna put the hurt on you But you better believe me when I tell you That I finally got the dirt on you You're busted! (busted!) Yeah, she's finally gonna see your lie This is how it's gonna be When she finds out that I was always right!");
+
+// Craving For Jelly: Story Content and Title
 function homicideFunction() {
-    document.getElementById("homicide").innerHTML = " the big corporate Kraft was shut down due to illegal Jell-O production. According to Darylll S.Parker, a Swat officer whose squad had raided the Kraft factory, 'We infiltrated the factory around 11:29am, where we found the perpetrators using illegal ingredient 'gelatin'... there were about 48 workers. My squad was able to arrest 39 workers,2 of which were undocumented illegal citizens'. The CEO of Kraft will have a press conference on May 20th, where Ms. Rosenfeld is said to release an appology. Although, she has made it clear that she will not be answering any further questions.";
+    document.getElementById("homicide").innerHTML = "On May 16th 2057, the big corporate Kraft was shut down due to illegal Jell-O production. According to Darylll S.Parker, a Swat officer whose squad had raided the Kraft factory, 'We infiltrated the factory around 11:29am, where we found the perpetrators using illegal ingredient 'gelatin'... there were about 48 workers. My squad was able to arrest 39 workers,2 of which were undocumented illegal citizens'. The CEO of Kraft will have a press conference on May 20th, where Ms. Rosenfeld is said to release an appology. Although, she has made it clear that she will not be answering any further questions.";
+    document.getElementById("titleStory").innerHTML = "The Craving For Jelly Still Exist?";
 }
+
+// Bright Future: Story Content and Title
 function camoflaugeFunction() {
-    document.getElementById("homicide").innerHTML = "Paragraph changed.";
+    document.getElementById("homicide").innerHTML = "The University of Transgression had discovered a new species, or evolution, of Jelly it has been named jellmeleon (Morone saxatilis). They scientist were sent on expedition to eastern Asia, where they initially were determined to study the local birds for any effects from polution, and they had stumbled upon a strange looking mango. to their astonishment, the mango was actually a jelly who was able to disguise itself as the fruit. Dr. Richtofen, a biology profesor, had told reporters, 'This new jelly species is an exciting transition, this species of jelly may become the new norm since as current research shows the previous species of jellies have had difficulty surviving the harsh environment. This new species of jellies may quite possibly be the biggest factor in the future generations of jellies, they might began an exponential growth. But who can say this is a definite, further research is required to make such giant claims.'the future for jellies is finally starting to look bright ever since the incident that brought a damper on jelly populations that we now call 'The Catalyzation'";
+    document.getElementById("titleStory").innerHTML = "A Bright Future for Jellies"
 }
+
+//Murder Convict: Story Content and Title
 function rebelFunction() {
-    document.getElementById("homicide").innerHTML = "Paragraph changed.";
+    document.getElementById("homicide").innerHTML = "A jelly prison that is located in the South Pole has reported a missing inmate, on May 18th 2057. The escapee's name is Jerry 'Kreed' Mondello, he is sentenced to life in prison for murdering several men, they were all caucasion elderly bald men. The guards on duty reported that there was scrap metal that had been sharpened. According to some reports by the other inmates and staff they had been finding many metalic tables and carts breaking because of missing parts. Investigators believe that he used the metal stakes as a way to dig through his cell wall. Currently, police officers are on high alert and are asking the public to be carful at this time. They are also asking for the publics help if anyone happens to see him to report it and if anyone knows where he might go to go to a local police station to share your info.";
+    document.getElementById("titleStory").innerHTML = "Murder Convict on the Loose"
 }
